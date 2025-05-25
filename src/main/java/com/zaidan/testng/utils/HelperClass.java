889406c2d1406b127e3ad2bfb3b36715f662e48b
naiasiaz <naiasitiazzahra@gmail.com>
2025-05-25 15:36:57 +0700

    Test Case 3 (masih error)

--- src/main/java/com/zaidan/testng/utils/HelperClass.java
+++ src/main/java/com/zaidan/testng/utils/HelperClass.java
@@ -3,6 +3,8 @@ package com.zaidan.testng.utils;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.HashMap;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HelperClass {
@@ -15,10 +17,20 @@ public class HelperClass {
     private HelperClass() {
 
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        // Gunakan profile baru yang kosong (temporary)
+        options.addArguments("user-data-dir=C:/temp/chrome-profile-zaidan");
+        options.addArguments("--no-first-run");
+        options.addArguments("--disable-save-password-bubble");
+        options.setExperimentalOption("prefs", new HashMap<String, Object>() {{
+            put("credentials_enable_service", false);
+            put("profile.password_manager_enabled", false);
+        }});
+
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
         driver.manage().window().maximize();
-
     }
 
     public static void openPage(String url) {
