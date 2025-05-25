f2d575d2b5198eb8f5c473008375ebe9abcde00c
Mahardika Pratama <mahardikapratama078@gmail.com>
2025-05-25 16:12:38 +0700

    Refactor HelperClass to use EdgeDriver instead of ChromeDriver; update file paths for consistency

--- src/main/java/com/zaidan/testng/utils/HelperClass.java
+++ src/main/java/com/zaidan/testng/utils/HelperClass.java
@@ -1,34 +1,32 @@
 package com.zaidan.testng.utils;
 
 import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.HashMap;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HelperClass {
 
     private static HelperClass helperClass;
-
     private static WebDriver driver;
     public final static int TIMEOUT = 10;
 
     private HelperClass() {
+        // Setup Edge driver
+        WebDriverManager.edgedriver().setup();
 
-        WebDriverManager.chromedriver().setup();
+        EdgeOptions options = new EdgeOptions();
+        driver = new EdgeDriver(options);
 
-        ChromeOptions options = new ChromeOptions();
-        // Gunakan profile baru yang kosong (temporary)
-        options.addArguments("user-data-dir=C:/temp/chrome-profile-zaidan");
-        options.addArguments("--no-first-run");
-        options.addArguments("--disable-save-password-bubble");
-        options.setExperimentalOption("prefs", new HashMap<String, Object>() {{
-            put("credentials_enable_service", false);
-            put("profile.password_manager_enabled", false);
-        }});
+        // Remove navigator.webdriver via JS
+        ((JavascriptExecutor) driver).executeScript(
+            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
+        );
 
-        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
         driver.manage().window().maximize();
     }
@@ -39,26 +37,18 @@ public class HelperClass {
 
     public static WebDriver getDriver() {
         return driver;
-
     }
 
     public static void setUpDriver() {
-
         if (helperClass == null) {
-
             helperClass = new HelperClass();
         }
     }
 
     public static void tearDown() {
-
         if (driver != null) {
-            driver.close();
             driver.quit();
         }
-
         helperClass = null;
-
     }
-
 }
